f4e82bebca81db52411aa4b7153374fa7664c767
henrique dantas <paulo.henrique@sapienstec.com>
2021-06-05 00:42:40 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import { Maps } from './components/mindMaps/maps';
+import { Maps } from './components/MindMaps/maps';
 import App from './App';
 import GlobalStyle from "./styles/global";
 
