b4994e25be33c801cc006356012a992b703b87a0
henrique dantas <paulo.henrique@sapienstec.com>
2021-06-10 23:44:27 -0300

    finalizando conteudo mapas mentais e quiz

--- src/Mocks/DataMaps.js
+++ src/Mocks/DataMaps.js
@@ -3,23 +3,23 @@ import * as images from '../assets';
 const DataCards = [
   {
     title: 'Grupo Python Kai',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    description: 'O mapa mental do Python Kai foi feito com intenção de trazer um visual agradável e organizado, em um layout relacionado a área do curso de Ciência da Computação. Utilizamos o esquema de cores da linguagem Python, que estamos estudando na cadeira de Algoritmos.',
     image: images.mapa1,      
     download: images.doc1,
     doc: true,
     
   },
   {
-    title: 'Grupo 2',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    title: 'Os Cérebros',
+    description: 'O grupo Os Cérebros, fizeram um trabalho bacana, apesar de faltar mais recursos de imagens e de ter ficado em formato de slide e não mapa mental, teve uma ótima proposta ao demostrar bem o assunto que foi proposto, não sendo insuficiente em informações. Uma palavra-chave para resumir esse grupo é, “Progredindo.”',
     image: images.mapa2,
     download: images.doc2,
     doc: true,
 
   },
   {
-    title: 'Grupo 3',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    title: 'Winx Club',
+    description: 'O grupo Winx Club, ao criarem o mapa mental com o tema “MACHISMO NA COMPUTAÇÃO”, visaram em demostrar de forma tanto ilustrativa, como escrita o fato de que as mulheres merecem seu lugar não só nessa área, como também, em todas as outras que bem quiserem. Na parte de estética do mapa mental, vemos que está fácil de ver e achar o que queremos, resumindo em uma palavra, “Radiante.”',
     image: images.mapa3,
     download: images.doc3,
     doc: true,
@@ -27,7 +27,7 @@ const DataCards = [
   },
   {
     title: 'THE LEGACY',
-    description: 'O grupo The Legacy, ao criarem o mapa mental com o tema “MACHISMO NA COMPUTAÇÃO”, visaram em demostrar de forma tanto ilustrativa, como escrita o fato de que as mulheres merecem seu lugar não só nessa área, como também, em todas as outras que bem quiserem. Na parte de estética do mapa mental, vemos que está fácil de ver e achar o que queremos, resumindo em uma palavra, está “radiante”',
+    description: 'O grupo The Legacy, nos surpreendeu pelo fato de mostrar imagens superintuitivas e de acordo com a nossa área. Eles foram super coerentes com a parte da junção do texto verbal e não verbal, assim a mescla não ficou confusa, mas sim, intuitiva. Uma palavra que resumiria o grupo seria, “Esplêndido.”',
     image: images.mapa4,
     download: images.doc4,
     doc: true,
